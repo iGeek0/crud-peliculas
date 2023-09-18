8eff0441178c831463d896345a839decf103850d
iGeek <jesus.cardenas.d@gmail.com>
2023-09-17 22:32:03 -0600

    pelicula inicial

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+console.log("Entro archivo index.js");
+
+class Pelicula {
+    constructor(titulo, estreno, genero, imagen, sinopsis) {
+        this.titulo = titulo;
+        this.estreno = estreno;
+        this.genero = genero;
+        this.imagen = imagen;
+        this.sinopsis = sinopsis;
+    }
+}
